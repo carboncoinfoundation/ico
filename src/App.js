965c2b0be06d139f1440e1808e059aa27cb27d25
Alasdair Blackwell <aliblackwell@gmail.com>
2017-10-29 15:11:52 +0000

    Basic working react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
+import CarboncoinSale from '../build/contracts/CarboncoinSale.json'
 import getWeb3 from './utils/getWeb3'
 
 import './css/oswald.css'
@@ -10,60 +10,39 @@ import './App.css'
 class App extends Component {
   constructor(props) {
     super(props)
-
     this.state = {
-      storageValue: 0,
-      web3: null
+      web3: null,
+      carboncoinSaleInstance: null,
+      account: null,
     }
   }
 
   componentWillMount() {
-    // Get network provider and web3 instance.
+    // Instantiate contract and get web3, account and saleInstance
     // See utils/getWeb3 for more info.
-
-    getWeb3
-    .then(results => {
-      this.setState({
-        web3: results.web3
-      })
-
-      // Instantiate contract once web3 provided.
-      this.instantiateContract()
+    const contract = require('truffle-contract')
+    const carboncoinSale = contract(CarboncoinSale)
+    getWeb3.then(results => {
+      carboncoinSale.setProvider(results.web3.currentProvider)
+      results.web3.eth.getAccounts((error, accounts) => {
+        carboncoinSale.deployed().then((carboncoinSaleInstance) => {
+          this.setState({
+            web3: results.web3,
+            account: accounts[0],
+            carboncoinSaleInstance
+          })
+        })
+      });
     })
     .catch(() => {
       console.log('Error finding web3.')
     })
   }
 
-  instantiateContract() {
-    /*
-     * SMART CONTRACT EXAMPLE
-     *
-     * Normally these functions would be called in the context of a
-     * state management library, but for convenience I've placed them here.
-     */
-
-    const contract = require('truffle-contract')
-    const simpleStorage = contract(SimpleStorageContract)
-    simpleStorage.setProvider(this.state.web3.currentProvider)
-
-    // Declaring this for later so we can chain functions on SimpleStorage.
-    var simpleStorageInstance
-
-    // Get accounts.
-    this.state.web3.eth.getAccounts((error, accounts) => {
-      simpleStorage.deployed().then((instance) => {
-        simpleStorageInstance = instance
-
-        // Stores a given value, 5 by default.
-        return simpleStorageInstance.set(5, {from: accounts[0]})
-      }).then((result) => {
-        // Get the value from the contract to prove it worked.
-        return simpleStorageInstance.get.call(accounts[0])
-      }).then((result) => {
-        // Update state with the result.
-        return this.setState({ storageValue: result.c[0] })
-      })
+  buyCoins = () => {
+    this.state.carboncoinSaleInstance.sendTransaction({
+      from: this.state.account,
+      value: this.state.web3.toWei(2, "ether"),
     })
   }
 
@@ -80,9 +59,10 @@ class App extends Component {
               <h1>Good to Go!</h1>
               <p>Your Truffle Box is installed and ready.</p>
               <h2>Smart Contract Example</h2>
+              { this.state.web3 ? <button onClick={this.buyCoins}>Buy Carboncoins</button> : null }
               <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
               <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
-              <p>The stored value is: {this.state.storageValue}</p>
+              <p>The stored value is: </p>
             </div>
           </div>
         </main>
