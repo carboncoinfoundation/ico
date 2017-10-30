22d4efda593c6aa235414a04fefa70db0f052c86
Alasdair Blackwell <aliblackwell@gmail.com>
2017-10-30 09:47:27 +0000

    Load everything before render

--- src/App.js
+++ src/App.js
@@ -12,8 +12,9 @@ class App extends Component {
     super(props)
     this.state = {
       web3: null,
-      carboncoinSaleInstance: null,
+      crowdsale: null,
       account: null,
+      tokenAddress: null,
     }
   }
 
@@ -25,11 +26,14 @@ class App extends Component {
     getWeb3.then(results => {
       carboncoinSale.setProvider(results.web3.currentProvider)
       results.web3.eth.getAccounts((error, accounts) => {
-        carboncoinSale.deployed().then((carboncoinSaleInstance) => {
-          this.setState({
-            web3: results.web3,
-            account: accounts[0],
-            carboncoinSaleInstance
+        carboncoinSale.deployed().then((crowdsale) => {
+          crowdsale.token().then(addr => {
+            this.setState({
+              web3: results.web3,
+              account: accounts[0],
+              crowdsale,
+              tokenAddress: addr,
+            })
           })
         })
       });
@@ -39,8 +43,10 @@ class App extends Component {
     })
   }
 
+
+
   buyCoins = () => {
-    this.state.carboncoinSaleInstance.sendTransaction({
+    this.state.crowdsale.sendTransaction({
       from: this.state.account,
       value: this.state.web3.toWei(2, "ether"),
     })
@@ -52,12 +58,10 @@ class App extends Component {
         <nav className="navbar pure-menu pure-menu-horizontal">
             <a href="#" className="pure-menu-heading pure-menu-link">Truffle Box</a>
         </nav>
-
         <main className="container">
           <div className="pure-g">
             <div className="pure-u-1-1">
               <h1>Good to Go!</h1>
-              <p>Your Truffle Box is installed and ready.</p>
               <h2>Smart Contract Example</h2>
               { this.state.web3 ? <button onClick={this.buyCoins}>Buy Carboncoins</button> : null }
               <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
