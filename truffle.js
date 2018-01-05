4e524f73ee64ddd0441fff1b60bdaf49cf1d7b55
Alasdair Blackwell <aliblackwell@gmail.com>
2018-01-05 14:19:25 +0000

    Rinkeby testnet

--- truffle.js
+++ truffle.js
@@ -4,6 +4,13 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      host: "localhost", // Connect to geth on the specified
+      port: 8545,
+      from: process.env.CREATOR_ACCOUNT, // default address to use for any transaction Truffle makes during migrations
+      network_id: 4,
+      gas: 4692388 // Gas limit used for deploys
     }
   }
 };
